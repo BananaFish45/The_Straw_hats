1e81fc28a894f2313bf158325a0d0f05c6651bcc
Selepe Sello <sbkskhalo.kq@gmail.com>
2022-10-02 01:33:13 +0200

    Tebogo Sculpture and Profile

---
+++ Profile/UploadProfile/UploadProfile.js
@@ -0,0 +1,24 @@
+/*
+
+const fs = require("fs");
+let usersjson = fs.readFileSync("users.json","utf-8");
+
+let users = JSON.parse(usersjson);
+
+// const arr = Array.from(users);
+
+users.push(obj);
+
+usersjson = JSON.stringify(users);
+
+fs.writeFileSync("users.json",usersjson,"utf-8");
+
+
+*/
+/*
+Bronze Animal Sculpture
+Checkers, Loftus, Pretoria
+4677.00
+Yung Mercy
+https://southafrica.co.za/images/leopard-1-786x523.jpg
+*/
---
+++ sculpturesGallery/UploadSculpture/UploadSculpture.js
@@ -0,0 +1,15 @@
+/*
+
+const fs = require("fs");
+let usersjson = fs.readFileSync("users.json","utf-8");
+
+let users = JSON.parse(usersjson);
+
+// const arr = Array.from(users);
+
+users.push(obj);
+
+usersjson = JSON.stringify(users);
+
+fs.writeFileSync("users.json",usersjson,"utf-8");
+*/
